1a5978e611345fb616d54b403d7d4eebf33312f8
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-10-06 12:19:51 +0200

    fix: CustomControl not touchable on mobile with isMenuOpen param
    see https://github.com/JedWatson/react-select/issues/3806#issuecomment-541325710

--- src/components/GroupsSelect/SelectBox/ControlDefault.jsx
+++ src/components/GroupsSelect/SelectBox/ControlDefault.jsx
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types'
 import { components } from 'cozy-ui/transpiled/react/SelectBox'
 
 const ControlDefault = ({ selectProps: { toggleMenu }, ...props }) => {
+  // onTouchStart is necessary on mobile
+  // see https://github.com/JedWatson/react-select/issues/3806#issuecomment-541325710
   return (
-    <div onClick={toggleMenu}>
+    <div onClick={toggleMenu} onTouchStart={toggleMenu}>
       <components.Control {...props} />
     </div>
   )
